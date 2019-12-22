3bd419e4523270f4ba70d727ea8d4c5a2022891d
guofucheng <627355704@qq.com>
2019-12-22 11:33:14 +0800

    feat:添加springBoot+websocket实现会话功能模块，添加.gitignore文件

---
+++ project-websocket/src/main/java/com/pers/guofucheng/MyApplication.java
@@ -0,0 +1,14 @@
+package com.pers.guofucheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springBoot+webSocket+VUE
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class);
+    }
+}
