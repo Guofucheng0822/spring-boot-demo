55da867c226893b8687f44af80ce1e9e4368d2b6
guofucheng <627355704@qq.com>
2020-04-21 23:55:28 +0800

    feat:新增springboot集成mongodb工程

---
+++ project-mongodb/src/main/java/com/pers/guofucheng/MyApplication.java
@@ -0,0 +1,11 @@
+package com.pers.guofucheng;
+
+/**
+ * @author guofucheng
+ * @title $type_name$
+ * @Description 描述
+ * @Date $date$ $time$
+ * @Copyright 2019-2020
+ */
+public class MyApplication {
+}
