7418e567b4b206f63efa3934225841d2ec010ab6
““guofucheng <guofucheng@e6yun.com>
2020-11-13 17:37:43 +0800

    feat:新增spi机制demo调用方法

--- spi-server/spi-application/src/main/java/com/pers/guofucheng/spi/SpiMain.java
+++ spi-server/spi-application/src/main/java/com/pers/guofucheng/spi/SpiMain.java
@@ -1,14 +1,24 @@
 package com.pers.guofucheng.spi;
 
+import sun.misc.Service;
+
 import java.util.Iterator;
 import java.util.ServiceLoader;
 
 public class SpiMain {
     public static void main(String[] args) {
+        // 1.通过ServiceLoader加载
         ServiceLoader<SpiService> loadedParsers = ServiceLoader.load(SpiService.class);
         Iterator<SpiService> iterator = loadedParsers.iterator();
         while (iterator.hasNext()){
             SpiService spiService = iterator.next();
+            spiService.sayHello("hhhhhhhhhh");
+        }
+        System.out.println("==========================================");
+        // 2.通过Service加载
+        Iterator<SpiService> providers = Service.providers(SpiService.class);
+        while (providers.hasNext()){
+            SpiService spiService = providers.next();
             spiService.sayHello("hhhhhhhhhh");
         }
     }
